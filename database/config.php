97511755e3b9af9d47d084ef96fcc89e3052ebe5
Ander <ander.robson1287@gmail.com>
2020-06-24 02:40:52 -0300

    FEA: Melhorias de conexão de banco de dados e de SEO

--- database/config.php
+++ database/config.php
@@ -1,6 +1,50 @@
 <?php
-define("DATABASE_HOST", "{HOST DO BANCO}");
-define("DATABASE_PORT", "{PORTA DE ACESSO AO BANCO}");
-define("DATABASE_DBNAME", "{NOME DO BANCO DE DADOS}");
-define("DATABASE_USER", "{NOME DO USUÁRIO DO BANCO DE DADOS}");
-define("DATABASE_PASSWORD", "{SENHA DO USUÁRIO DO BANCO DE DADOS}");
+
+    /**
+     * Database config
+     */
+    define("DATABASE", [
+        "HOST" => "localhost",
+        "PORT" => "3306",
+        "DBNAME" => "laravel",
+        "USER" => "root",
+        "PASSWORD" => ""
+    ]);
+
+    /**
+     * Site config
+     */
+    define("SITE", [
+        "NAME" => "Plataforma de desenvolvimento Dev",
+        "SHORT_NAME" => "Site Dev",
+        "DESCRIPTION" => "Aprendendo a programar com PHP do jeito certo !",
+        "DOMAIN" => "wwww.sitedev.com.br",
+        "LOCALE" => "pt_BR",
+        "PATH" => "site-dev"
+    ]);
+
+    /**
+     *  Social config
+     */
+    define("SOCIAL", [
+       "FACEBOOK_PAGE" => "ander.robson",
+       "FACEBOOK_AUTHOR" => "ander.robson",
+       "FACEBOOK_APP_ID" => "123456",
+       "TWITTER_CREATOR" => "@ilikeAsking",
+       "TWITTER_SITE" => "@ilikeAsking",
+    ]);
+
+    /**
+     *  Mail connect
+     */
+    define("MAIL", []);
+
+    /**
+     *  Social login: Facebook
+     */
+    define("FCEBOOK_LOGIN", []);
+
+    /**
+     *  Social login: Google
+     */
+    define("GOOGLE_LOGIN", []);
